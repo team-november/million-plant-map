27ba1f53737ff794602b0552053737f12808acf7
ben philps <bp413@cam.ac.uk>
2019-02-22 18:27:48 +0000

    Update IndexFetcher.java
    Geo codes can now be fetched individually using the fetchGeoCodeList and fetchGeoCode methods

--- src/indexesAndCodes/IndexFetcher.java
+++ src/indexesAndCodes/IndexFetcher.java
@@ -7,7 +7,31 @@ public class IndexFetcher {
     // for the two lists of family and genus, it fetches the BH index, and the FE index
     // and the geo codes.
     // will extend this to cover the other indexing schemes that there are
-    public static String[][] fetchAll(String[] families, String[] genuses, String[] geoCodes){
+    public static String[] fetchIndexesCodes(String family, String genus){
+
+        String [] results = new String[2];
+
+        BHIndexFetcher BHfetcher = new BHIndexFetcher();
+        String r0 = BHfetcher.fetchCode(family.toLowerCase());
+        if(r0 != "") r0 = "BH: " + r0 + ", ";
+
+        FEIndexFetcher FEfetcher = new FEIndexFetcher();
+        String r1 = BHfetcher.fetchCode(genus.toLowerCase());
+        if(r1 != "") r1 = "FE: " + r0 + ", ";
+
+        results[0] = r0+r1;
+
+
+        return results;
+
+    }
+
+    private String getOnlineGeoCode(String family, String genus){
+        return "";
+    }
+
+
+    private static String[][] fetchAll(String[] families, String[] genuses, String[] geoCodes){
 
         String [][] results = new String[3][];
         BHIndexFetcher BHfetcher = new BHIndexFetcher();
@@ -21,6 +45,23 @@ public class IndexFetcher {
 
         return results;
 
+    }
+
+    // Todo new method need to say its here
+    // Gets interpreted value for a list of codes
+    public static String fetchGeoCodeList(String[] codes){
+
+        // instantiate a geo code fetcher and return an string of all the combined codes, interpreted
+        GeoCodeFetcher geoCodeFetcher = new GeoCodeFetcher();
+        return geoCodeFetcher.fetchCodesSingleString(codes);
+
+    }
+
+    // Todo new method added say that it is here
+    private static String fetchGeoCode(String code){
+
+        GeoCodeFetcher geoCodeFetcher = new GeoCodeFetcher();
+        return geoCodeFetcher.fetchCodesSingleString(new String[]{code});
     }
 
 }
