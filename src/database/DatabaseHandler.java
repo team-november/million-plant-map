80c15b909f6abbd12881c35761c9718cd46d7a5f
kamilest <stankeviciute.kamile@gmail.com>
2019-02-23 20:37:54 +0000

    Added stubs for basic database query functionality.

---
+++ src/database/DatabaseHandler.java
@@ -0,0 +1,28 @@
+package database;
+
+public class DatabaseHandler {
+  public void putSynonym(Synonym synonym) {
+
+  }
+
+  public Synonym getSynonym(String name) {
+
+    return null;
+  }
+
+  public Location getLocation(String code) {
+
+    return null;
+  }
+
+  public Family getFamily(String name) {
+
+    return null;
+  }
+
+  public Genus getGenus(String name) {
+
+    return null;
+  }
+
+}
