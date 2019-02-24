3b09d760bf793b618951360a94fad26a74067f24
kamilest <stankeviciute.kamile@gmail.com>
2019-02-24 15:48:34 +0000

    Added method documentation and minor improvements.

--- src/database/DatabaseHandler.java
+++ src/database/DatabaseHandler.java
@@ -9,7 +9,14 @@ import java.util.ArrayList;
 public class DatabaseHandler {
   private Connection  connection = ConnectionHandler.getConnectionHandler()
                                                     .getConnection();
-
+  /**
+   * Inserts the prepared Synonym as a record in the herbarium database.
+   * 
+   * A Synonym corresponds to a row in the `synonyms` table of the
+   * `herbarium_index`.
+   * 
+   * @param synonym the Synonym to be inserted.
+   */
   public void insertSynonym(Synonym synonym) {
     String query = "INSERT INTO synonyms"
       + " (species_name, family_name, index_scheme, family_number, "
@@ -17,14 +24,14 @@ public class DatabaseHandler {
       + " VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
     
     try (PreparedStatement ps = connection.prepareStatement(query)) {
-      ps.setString(1, synonym.getName().toLowerCase());
-      ps.setString(2, synonym.getScheme().toString().toLowerCase());
-      ps.setString(3, synonym.getFamilyName().toLowerCase());
-      ps.setString(4, synonym.getFamilyNumber().toLowerCase());
-      ps.setString(5, synonym.getGenusNumber().toLowerCase());
+      ps.setString(1, synonym.getName().toLowerCase().trim());
+      ps.setString(2, synonym.getScheme().toString().toLowerCase().trim());
+      ps.setString(3, synonym.getFamilyName().toLowerCase().trim());
+      ps.setString(4, synonym.getFamilyNumber().toLowerCase().trim());
+      ps.setString(5, synonym.getGenusNumber().toLowerCase().trim());
       ps.setBoolean(6, synonym.isAccepted());
       ps.setBoolean(7, synonym.isBasionym());
-      ps.setString(8, synonym.getNote());
+      ps.setString(8, synonym.getNote().trim());
       ps.executeUpdate();
     } catch (SQLException e) {
       System.err.println("Could not execute insertSynonym query.");
@@ -32,7 +39,17 @@ public class DatabaseHandler {
     }
   }
 
-  public ArrayList<Synonym> getSynonymsByName(String name) {
+  /**
+   * Retrieves all records associated with a given species name.
+   * 
+   * This method queries the `synonyms` table of `herbarium_index`
+   * and returns the records that have a matching `species_name`
+   * attribute. The rows are converted to Synonym objects.
+   *  
+   * @param name the species name.
+   * @return the list of synonym records matching this species name.
+   */
+  public ArrayList<Synonym> getAllSynonymsByName(String name) {
     ArrayList<Synonym> synonyms = new ArrayList<>(); 
 
     String query = "SELECT "
@@ -42,7 +59,7 @@ public class DatabaseHandler {
       + " WHERE species_name = ?";
     
     try (PreparedStatement ps = connection.prepareStatement(query)) {
-      ps.setString(1, name);
+      ps.setString(1, name.trim());
       ResultSet resultSet = ps.executeQuery();
 
       // Creates a Synonym object for each row in the ResultSet.
@@ -66,11 +83,28 @@ public class DatabaseHandler {
     return synonyms;
   }
 
+  /**
+   * Retrieves the first record matching the given species name.
+   * 
+   * See {@link #getAllSynonymsByName(String) getAllSyonymsByName} method.
+   * 
+   * @param name the species name.
+   * @return the first synonym record matching this species name.
+   */
   public Synonym getFirstSynonymByName(String name) {
-    ArrayList<Synonym> synonyms = getSynonymsByName(name);
+    ArrayList<Synonym> synonyms = getAllSynonymsByName(name);
     return (synonyms != null) ? synonyms.get(0) : null;
   }
 
+  /**
+   * Retrieves the first location record matching the given geocode.
+   * 
+   * The method queries the `locations` table in `herbarium_index` to 
+   * retrieve the definition (explanation) of a geographical code.
+   * 
+   * @param code the geographical code.
+   * @return the record matching the code as a {@link Location}.
+   */
   public Location getLocationByCode(String code) {
     Location location = null;
 
@@ -97,25 +131,50 @@ public class DatabaseHandler {
     return location;
   }
 
+  /**
+   * Returns the family numbers for given family name.
+   * 
+   * This queries the `families_*` tables in `herbarium_index`
+   * for every indexing scheme and returns the correspoding family
+   * numbers, wrapped in a {@link Family} object.
+   * 
+   * The schemes that are queried are provided by the {@link IndexScheme}
+   * type (and usually excludes {@link IndexScheme#OTHER}).
+   * 
+   * @param name name of the family
+   * @return the list of {@link Family} records containing family numbers.
+   */
   public ArrayList<Family> getFamiliesByFamilyName(String name) {
     ArrayList<Family> families = new ArrayList<>();
     
-    families.addAll(getFamiliesByScheme(name, IndexScheme.GB_AND_I));
-    families.addAll(getFamiliesByScheme(name, IndexScheme.FLORA_EUROPAEA));
-    families.addAll(getFamiliesByScheme(name, IndexScheme.BENTHAM_HOOKER));
+    for (IndexScheme scheme : IndexScheme.values())
+      families.addAll(getFamiliesByScheme(name, scheme));
 
     return families;
   }
 
+  /**
+   * Returns the family number of a given name in a given indexing scheme.
+   * 
+   * This queries the appropriate `families_*` table in `herbarium_index`
+   * that contains family numbers for the requested indexing scheme 
+   * (see {@link IndexScheme}).
+   * 
+   * @param name name of the family
+   * @param scheme the indexing scheme under which the family number is needed
+   * @return the list of {@link Family} records containing family numbers. 
+   * Returns null for the {@link IndexScheme#OTHER} scheme which is reserverd
+   * for the `synonyms` table.
+   */
   private ArrayList<Family> getFamiliesByScheme(String name, 
-                                                 IndexScheme scheme) {
+                                                IndexScheme scheme) {
     ArrayList<Family> families = new ArrayList<>();
     
     String tableName;
     if (scheme == IndexScheme.GB_AND_I) tableName = "families_gbi";
     else if (scheme == IndexScheme.BENTHAM_HOOKER) tableName = "families_bh";
     else if (scheme == IndexScheme.FLORA_EUROPAEA) tableName = "families_fe";
-    else return families;
+    else return null;
 
     String query = "SELECT "
       + " (family_name, family_number)"
@@ -123,7 +182,7 @@ public class DatabaseHandler {
       + " WHERE family_name = ?";
 
     try (PreparedStatement ps = connection.prepareStatement(query)) {
-      ps.setString(1, name);
+      ps.setString(1, name.trim());
 
       ResultSet resultSet = ps.executeQuery();
 
@@ -143,16 +202,38 @@ public class DatabaseHandler {
     return families;
   }
 
+  /**
+   * Returns the genus numbers of a given name for supported indexing schemes.
+   * 
+   * This queries all `genera_*` tables in `herbarium_index`
+   * that contain genus numbers for the supported indexing schemes 
+   * (see {@link IndexScheme}).
+   * 
+   * @param name name of the genus
+   * @return the list of {@link Genus} records containing genus numbers.
+   */
   public ArrayList<Genus> getGeneraByGenusName(String name) {
     ArrayList<Genus> genera = new ArrayList<>();
     
-    genera.addAll(getGeneraByScheme(name, IndexScheme.GB_AND_I));
-    genera.addAll(getGeneraByScheme(name, IndexScheme.FLORA_EUROPAEA));
-    genera.addAll(getGeneraByScheme(name, IndexScheme.BENTHAM_HOOKER));
+    for (IndexScheme scheme : IndexScheme.values())
+      genera.addAll(getGeneraByScheme(name, scheme));
 
     return genera;
   }
 
+  /**
+   * Returns the genus number of the genus under a given indexing scheme.
+   * 
+   * This queries the appropriate `genera_*` table in `herbarium_index`
+   * that contains genus numbers for the requested indexing scheme 
+   * (see {@link IndexScheme}).
+   * 
+   * @param name name of the genus
+   * @param scheme the indexing scheme under which the genus number is needed
+   * @return the list of {@link Genus} records containing genus numbers. 
+   * Returns null for the {@link IndexScheme#OTHER} scheme which is reserverd
+   * for the `synonyms` table.
+   */
   private ArrayList<Genus> getGeneraByScheme(String name,
                                              IndexScheme scheme) {
     ArrayList<Genus> genera = new ArrayList<>();
@@ -161,7 +242,7 @@ public class DatabaseHandler {
     if (scheme == IndexScheme.GB_AND_I) tableName = "genera_gbi";
     else if (scheme == IndexScheme.BENTHAM_HOOKER) tableName = "genera_bh";
     else if (scheme == IndexScheme.FLORA_EUROPAEA) tableName = "genera_fe";
-    else return genera;
+    else return null;
 
     String query = "SELECT "
       + " (genus_name, family_number, genus_number)"
@@ -169,7 +250,7 @@ public class DatabaseHandler {
       + " WHERE genus_name = ?";
 
     try (PreparedStatement ps = connection.prepareStatement(query)) {
-      ps.setString(1, name);
+      ps.setString(1, name.trim());
 
       ResultSet resultSet = ps.executeQuery();
 
@@ -189,23 +270,74 @@ public class DatabaseHandler {
 
     return genera;
   }
-
+  
+  /**
+   * Returns the list of {@link Location} records where the genus is located.
+   * 
+   * The result corresponds to the records in the `synonyms_locations` table
+   * of `herbarium_index` that match the provided genus name.
+   * 
+   * @param genusName the genus name
+   * @return the locations of the genus
+   */
   public ArrayList<Location> getLocationsByGenusName(String genusName) {
     return getLocationsByName(genusName.split(" ")[0]);
   }
 
+  /**
+   * Returns the list of {@link Location} records where the genus is located.
+   * 
+   * The result corresponds to the records in the `synonyms_locations` table
+   * of `herbarium_index` that match the name of the genus provided.
+   * 
+   * @param genus the genus
+   * @return the locations of the genus
+   */
   public ArrayList<Location> getLocationsByGenus(Genus genus) {
     return getLocationsByName(genus.getGenusName().split(" ")[0]);
   }
 
+  /**
+   * Returns the list of {@link Location} records where the species is located.
+   * 
+   * The result corresponds to the records in the `synonyms_locations` table
+   * of `herbarium_index` that match the genus name of the species provided. 
+   * This means that the locations are returned for the entire genus of the
+   * given species, and not the exact species.
+   * 
+   * @param speciesName the species name
+   * @return the locations of the genus of the species
+   */
   public ArrayList<Location> getLocationsBySynonymName(String speciesName) {
     return getLocationsByName(speciesName.split(" ")[0]);
   }
 
+  /**
+   * Returns the list of {@link Location} records where the species is located.
+   * 
+   * The result corresponds to the records in the `synonyms_locations` table
+   * of `herbarium_index` that match the genus name of the species provided. 
+   * This means that the locations are returned for the entire genus of the
+   * given species, and not the exact species.
+   * 
+   * @param synonym the synonym containing the species name
+   * @return the locations of the genus of the species
+   */
   public ArrayList<Location> getLocationsBySynonym(Synonym synonym) {
     return getLocationsByName(synonym.getName().split(" ")[0]);
   }
 
+  /**
+   * Returns the list of {@link Location} records that match a given genus name.
+   * 
+   * The result corresponds to the records in the `synonyms_locations` table
+   * of `herbarium_index`. The method takes in an arbitrary name (presumably 
+   * genus) and finds all genus-location records that start with the name 
+   * provided.
+   * 
+   * @param name the name of the genus
+   * @return the locations of the genus of the species
+   */
   private ArrayList<Location> getLocationsByName(String name) {
     ArrayList<Location> locations = new ArrayList<>();
 
@@ -236,11 +368,21 @@ public class DatabaseHandler {
     return locations;
   }
 
+  /**
+   * Converts the given string into the corresponding {@link IndexScheme}.
+   * 
+   * @param scheme the name of scheme
+   * @return the {@link IndexScheme} type
+   */
   private IndexScheme stringToIndexScheme(String scheme) {
-    if (scheme.equals("GB_AND_I")) return IndexScheme.GB_AND_I;
-    else if (scheme.equals("FLORA_EUROPAEA")) return IndexScheme.FLORA_EUROPAEA;
-    else if (scheme.equals("BENTHAM_HOOKER")) return IndexScheme.BENTHAM_HOOKER;
-    else return IndexScheme.OTHER;
+    if (scheme.equalsIgnoreCase("GB_AND_I")) 
+      return IndexScheme.GB_AND_I;
+    else if (scheme.equalsIgnoreCase("FLORA_EUROPAEA")) 
+      return IndexScheme.FLORA_EUROPAEA;
+    else if (scheme.equalsIgnoreCase("BENTHAM_HOOKER")) 
+      return IndexScheme.BENTHAM_HOOKER;
+    else 
+      return IndexScheme.OTHER;
   }
 
 }
