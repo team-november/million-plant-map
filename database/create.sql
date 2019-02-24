29d72dea0d915e4e3ed5cfccf0d961ae358eab99
kamilest <stankeviciute.kamile@gmail.com>
2019-02-24 16:48:05 +0000

    Updated DDL for `herbarium_index` database.

--- database/create.sql
+++ database/create.sql
@@ -1,108 +1,83 @@
-CREATE TABLE `herbarium_index`.`synonyms` (
-  `name` VARCHAR(45) NOT NULL,
-  `species_id` INT NOT NULL,
-  `family_name` VARCHAR(45) NOT NULL,
-  `is_accepted` TINYINT NOT NULL DEFAULT 0,
-  `is_basionym` TINYINT NULL DEFAULT 0,
-  `note` VARCHAR(45) NULL DEFAULT NULL,
-  PRIMARY KEY (`name`),
-  UNIQUE INDEX `species_name_UNIQUE` (`name` ASC) VISIBLE);
+CREATE TABLE `families_bh` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `family_name` varchar(45) NOT NULL,
+  `family_number` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  KEY `name_idx` (`family_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=565 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
 
-CREATE TABLE `herbarium_index`.`families` (
-  `name` VARCHAR(45) NOT NULL,
-  `gbi_index` VARCHAR(45) NULL,
-  `fe_index` VARCHAR(45) NULL,
-  `bh_index` VARCHAR(45) NULL,
-  `true_index` VARCHAR(45) NULL,
-  PRIMARY KEY (`name`),
-  UNIQUE INDEX `name_UNIQUE` (`name` ASC) VISIBLE);
+CREATE TABLE `families_fe` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `family_name` varchar(45) NOT NULL,
+  `family_number` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  KEY `name_idx` (`family_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=196 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
 
-CREATE TABLE `herbarium_index`.`locations` (
-  `code` VARCHAR(45) NOT NULL,
-  `code_definition` VARCHAR(45) NULL DEFAULT NULL,
+CREATE TABLE `families_gbi` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `family_name` varchar(45) NOT NULL,
+  `family_number` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  KEY `name_idx` (`family_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+CREATE TABLE `genera_bh` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `genus_name` varchar(45) NOT NULL,
+  `family_number` varchar(45) DEFAULT NULL,
+  `genus_number` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  KEY `genus_name_idx` (`genus_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+CREATE TABLE `genera_fe` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `genus_name` varchar(45) NOT NULL,
+  `family_number` varchar(45) DEFAULT NULL,
+  `genus_number` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  KEY `genus_name_idx` (`genus_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=1240 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+CREATE TABLE `genera_gbi` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `genus_name` varchar(45) NOT NULL,
+  `family_number` varchar(45) DEFAULT NULL,
+  `genus_number` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  KEY `genus_name_idx` (`genus_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+CREATE TABLE `genera_locations` (
+  `genus_name` varchar(45) NOT NULL,
+  `location_code` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+CREATE TABLE `locations` (
+  `code` varchar(45) NOT NULL,
+  `code_definition` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`code`),
-  UNIQUE INDEX `code_UNIQUE` (`code` ASC) VISIBLE);
+  UNIQUE KEY `code_UNIQUE` (`code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
 
-ALTER TABLE `herbarium_index`.`synonyms` 
-ADD INDEX `name_idx` (`family_name` ASC) VISIBLE;
-;
-ALTER TABLE `herbarium_index`.`synonyms` 
-ADD CONSTRAINT `name`
-  FOREIGN KEY (`family_name`)
-  REFERENCES `herbarium_index`.`families` (`name`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
+CREATE TABLE `synonyms` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `species_name` varchar(45) NOT NULL,
+  `family_name` varchar(45) NOT NULL,
+  `index_scheme` varchar(45) DEFAULT NULL,
+  `family_number` varchar(45) NOT NULL,
+  `genus_number` varchar(45) DEFAULT NULL,
+  `is_accepted` tinyint(1) DEFAULT NULL,
+  `is_basionym` tinyint(1) DEFAULT NULL,
+  `note` mediumtext,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
 
-ALTER TABLE `herbarium_index`.`synonyms` 
-DROP FOREIGN KEY `name`;
-ALTER TABLE `herbarium_index`.`synonyms` 
-DROP INDEX `name_idx` ;
-;
-
-ALTER TABLE `herbarium_index`.`families` 
-ADD COLUMN `family_id` INT NOT NULL AUTO_INCREMENT FIRST,
-DROP PRIMARY KEY,
-ADD PRIMARY KEY (`family_id`),
-ADD UNIQUE INDEX `id_UNIQUE` (`family_id` ASC) VISIBLE,
-DROP INDEX `name_UNIQUE` ;
-;
-
-CREATE TABLE `herbarium_index`.`families_bh` (
-  `name` VARCHAR(45) NOT NULL,
-  `index` VARCHAR(45) NOT NULL);
-
-CREATE TABLE `herbarium_index`.`families_gbi` (
-  `name` VARCHAR(45) NOT NULL,
-  `value` VARCHAR(45) NOT NULL,
-  INDEX `name_idx` (`name` ASC) VISIBLE);
-
-
-CREATE TABLE `herbarium_index`.`families_fe` (
-  `name` VARCHAR(45) NOT NULL,
-  `value` VARCHAR(45) NOT NULL,
-  INDEX `name_idx` (`name` ASC) VISIBLE);
-
-
-ALTER TABLE `herbarium_index`.`families` 
-DROP COLUMN `bh_index`,
-DROP COLUMN `fe_index`,
-DROP COLUMN `gbi_index`,
-DROP COLUMN `family_id`,
-ADD COLUMN `type` VARCHAR(45) NULL AFTER `name`,
-CHANGE COLUMN `true_index` `value` VARCHAR(45) NOT NULL ,
-ADD INDEX `name_idx` (`name` ASC) VISIBLE,
-DROP INDEX `id_UNIQUE` ,
-DROP PRIMARY KEY;
-, RENAME TO  `herbarium_index`.`families_true` ;
-
-
-ALTER TABLE `herbarium_index`.`synonyms` 
-DROP COLUMN `species_id`,
-ADD COLUMN `family_index_type` VARCHAR(45) NULL AFTER `family_name`,
-ADD COLUMN `family_index_value` VARCHAR(45) NOT NULL AFTER `family_index_type`,
-ADD COLUMN `genus_index_type` VARCHAR(45) NULL AFTER `family_index_value`,
-ADD COLUMN `genus_index_value` VARCHAR(45) NULL AFTER `genus_index_type`;
-
-
-ALTER TABLE `herbarium_index`.`synonyms` 
-ADD COLUMN `id` INT NOT NULL AUTO_INCREMENT FIRST,
-DROP PRIMARY KEY,
-ADD PRIMARY KEY (`id`),
-ADD UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
-DROP INDEX `species_name_UNIQUE` ;
-;
-
-CREATE TABLE `herbarium_index`.`genera_bh` (
-  `name` VARCHAR(45) NOT NULL,
-  `value` VARCHAR(45) NOT NULL,
-  INDEX `name_idx` (`name` ASC) VISIBLE);
-
-CREATE TABLE `herbarium_index`.`genera_fe` (
-  `name` VARCHAR(45) NOT NULL,
-  `value` VARCHAR(45) NOT NULL,
-  INDEX `name_idx` (`name` ASC) VISIBLE);
-
-CREATE TABLE `herbarium_index`.`genera_gbi` (
-  `name` VARCHAR(45) NOT NULL,
-  `value` VARCHAR(45) NOT NULL,
-  INDEX `name_idx` (`name` ASC) VISIBLE);
