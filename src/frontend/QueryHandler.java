943d81a5a4b2891d7425dd83aaf0511862492f92
andrewcoalter <38883607+andrewcoalter@users.noreply.github.com>
2019-02-26 20:54:51 +0000

    Fixed geoCodes interpretation bug

--- src/frontend/QueryHandler.java
+++ src/frontend/QueryHandler.java
@@ -3,6 +3,7 @@ package frontend;
 import api.APIServiceImpl;
 import api.QueryResult;
 import api.Species;
+import database.DatabaseHandler;
 import indexes.IndexFetcher;
 import indexes.PlantGeoCodeFetcher;
 
@@ -13,7 +14,8 @@ public class QueryHandler {
 
 
     public QueryHandlerResult query(String name) {
-        //TODO: Add interaction with database
+        //Code for whe database connection is working:
+        //DatabaseHandler dBH = DatabaseHandler.getInstance();
         APIServiceImpl api = APIServiceImpl.getInstance();
         QueryResult queryResult = api.getAcceptedNameAndSynonyms(name);
         if(!queryResult.iterator().hasNext()){
@@ -25,6 +27,14 @@ public class QueryHandler {
         for (Species species : queryResult) {
             String codes = indexFetcher.fetchIndexes(species.getFamily(), species.getGenus());
             species.setCodes(codes);
+            //Code for when the database connection is working:
+            /*if(dBH.getFirstSynonymByName(name) == null){
+                species.setIsInHerbarium(false);
+            }
+            else{
+                species.setIsInHerbarium(true);
+            }*/
+            //Instead of this line:
             species.setIsInHerbarium(false);
             toReturn.add(species);
         }
